dcbcda36204695272f23b22c1a4614cb81b6c976
phasenull <55253035+phasenull@users.noreply.github.com>
2024-07-11 20:51:50 +0300

    workflow fix

--- tests/Static.test.ts
+++ tests/Static.test.ts
@@ -1,5 +1,5 @@
 import {test,expect} from "vitest"
-import {KentKart} from "../src"
+import {KentKart} from "../dist"
 test("KentKart.getCityList",async ()=>{
 	const data = await KentKart.getCityList()
 	expect(data.map((e)=>e.id)).toContain("004")
