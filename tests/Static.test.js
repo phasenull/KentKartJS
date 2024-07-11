8e0bfa2718ca5bd14b19acb6c6f5748c1c00780e
phasenull <55253035+phasenull@users.noreply.github.com>
2024-07-11 21:03:04 +0300

    tests update

--- tests/Static.test.js
+++ tests/Static.test.js
@@ -1,6 +1,11 @@
-import {test,expect} from "vitest"
-import {KentKart} from "../src"
-test("KentKart.getCityList",async ()=>{
+import fetch from "node-fetch"
+if (!global?.fetch) {
+	global.fetch = fetch
+}
+
+import { test, expect } from "vitest"
+import { KentKart } from "../src"
+test("KentKart.getCityList", async () => {
 	const data = await KentKart.getCityList()
-	expect(data.map((e)=>e.id)).toContain("004")
+	expect(data.map((e) => e.id)).toContain("004")
 })
